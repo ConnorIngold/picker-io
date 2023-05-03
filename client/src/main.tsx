f928ae8a4f41d4f9d3a6c50029b9827afdd36981
connoringold <connoringold@gmail.com>
2023-05-03 23:08:23 +0100

    frontend test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 import { Provider } from '@shopify/app-bridge-react'
 const host = 'connor-charle-dev.myshopify.com'
-const apiKey = process.env.SHOPIFY_API_KEY || 'e0f06376ae46ef041a82b7b6f5b96f32'
+const apiKey = 'e0f06376ae46ef041a82b7b6f5b96f32'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
