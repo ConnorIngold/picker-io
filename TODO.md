2a9b96dc8cd8d75b3cb69e12ea4be696e6bca498
connoringold <connoringold@gmail.com>
2023-05-03 23:02:06 +0100

    frontend test

--- TODO.md
+++ TODO.md
@@ -8,3 +8,8 @@ Building a Shopify app starter kit
 - [X] Dockerize and deploy to Railway
   - [X] Update the app URL in the Shopify app settings
 - [x] Create a DB model to store the session data
+
+
+## backlog
+- [] Make it so the app is embedded in the admin on first install
+
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Provider } from '@shopify/app-bridge-react'
+const host = 'connor-charle-dev.myshopify.com'
+const apiKey = process.env.SHOPIFY_API_KEY || 'e0f06376ae46ef041a82b7b6f5b96f32'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<Provider config={{ apiKey, host, forceRedirect: true }}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 )
