8aaa21fe9f589872c39d5bbca1ffb157009affd6
connoringold <connoringold@gmail.com>
2023-05-03 23:29:00 +0100

    frontend test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from '@shopify/app-bridge-react'
-const host = new URLSearchParams(location.search).get("host") || 'https://connor-charle-dev.myshopify.com'
+const host = new URLSearchParams(location.search).get('host') || 'https://connor-charle-dev.myshopify.com'
 const apiKey = 'e0f06376ae46ef041a82b7b6f5b96f32'
-
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
@@ -16,7 +13,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 		</Provider>
 	</React.StrictMode>
 )
-
-
-
-https://connor-charle-dev.myshopify.com/admin/oauth/post_grant?access_change_uuid=1ea442a9-5998-4e4f-8601-fffa22f2a3a9&client_id=e0f06376ae46ef041a82b7b6f5b96f32
