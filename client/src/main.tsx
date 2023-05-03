f0a5afd8bc15130e48c58a9bba2d1a816d9e56c4
connoringold <connoringold@gmail.com>
2023-05-03 23:17:33 +0100

    frontend test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Provider } from '@shopify/app-bridge-react'
-const host = 'picker-io-production.up.railway.app'
+const host = 'https://connor-charle-dev.myshopify.com'
 const apiKey = 'e0f06376ae46ef041a82b7b6f5b96f32'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<Provider config={{ apiKey, host, forceRedirect: true }}>
+		<Provider config={{ apiKey, host: host, forceRedirect: true }}>
 			<App />
 		</Provider>
 	</React.StrictMode>
