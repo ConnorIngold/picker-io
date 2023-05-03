1b8fd2fe78fa78165dc12c0d438568d077e15fcc
connoringold <connoringold@gmail.com>
2023-05-03 19:31:14 +0100

    bug fix

--- dist/app.js
+++ dist/app.js
@@ -90,6 +90,7 @@ app.get('/', async (req, res) => {
         }
     }
 });
+app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'client/dist')));
 app.get('/frontend', async (req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../', 'client/dist', 'index.html'));
 });
--- src/app.ts
+++ src/app.ts
@@ -101,6 +101,8 @@ app.get('/', async (req: Request, res: Response) => {
 	}
 })
 
+app.use(express.static(path.join(__dirname, '..', 'client/dist')))
+
 app.get('/frontend', async (req: Request, res: Response) => {
 	res.sendFile(path.join(__dirname, '../', 'client/dist', 'index.html'))
 })
